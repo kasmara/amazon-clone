e883cb587f2a0ddc4c4a9f9c40c8a96a91ce1d09
Cascadio <briankasmara@yahoo.co.nz>
2021-09-20 01:13:12 +1200

    Added home and prodcut components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.js";
+import Home from "./Home";
 
 function App() {
   return (
     // BEM convention
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
