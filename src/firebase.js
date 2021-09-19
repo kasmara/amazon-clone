e0dca570cf62aac816c42d1b0e39916564cde62b
Cascadio <briankasmara@yahoo.co.nz>
2021-09-20 00:39:27 +1200

    Adding header files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDyxqCU-7se01c_L_D97u-Bxx-YQCqUiNg",
+    authDomain: "clone-14ca4.firebaseapp.com",
+    projectId: "clone-14ca4",
+    storageBucket: "clone-14ca4.appspot.com",
+    messagingSenderId: "901640340428",
+    appId: "1:901640340428:web:cb65ea9ffc99d506e31369",
+    measurementId: "G-YEY6R8SGYN"
+  };
