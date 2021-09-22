d2e2a09f8926c04b8f9e3c31b65dec2b3ef38767
Cascadio <briankasmara@yahoo.co.nz>
2021-09-23 00:22:05 +1200

    Login page complete

---
+++ src/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form action="">
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign-in
+          </button>
+        </form>
+        <p>
+          By signing in, you agree to Amazon Clone's Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice, and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button className="login__registerButton" onClick={register}>
+          Create your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
