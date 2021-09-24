152d6e5a247c927a6c3a923d86dbd364bbf674b5
Cascadio <briankasmara@yahoo.co.nz>
2021-09-25 02:04:03 +1200

    Completed cloud function
    Completed cloud function and stripe payment processing

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Jd9KIIJSm6deQo34Zy2wnbWXz6jN5yTfvaylOwSLHweC1CxbnamxpaZtP8meD7eJb29xtZp0MsF9uRgFW7K7S3700ErtH7ftk"
+);
+
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API route
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/clone-14ca4/us-central1/api
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "...", // the API url (cloud function url)
+  baseURL: "http://localhost:5001/clone-14ca4/us-central1/api", // the API url (cloud function url)
 });
 
 export default instance;
