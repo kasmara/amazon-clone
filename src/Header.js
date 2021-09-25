3c7a7f36d0d06e655b9eca67562a11c56054d317
Cascadio <briankasmara@yahoo.co.nz>
2021-09-25 18:52:47 +1200

    Orders page and order comp finished
    Orders page and order comp finished

--- src/Header.js
+++ src/Header.js
@@ -31,17 +31,19 @@ function Header() {
         <Link to={!user && "/login"}>
           <div className="header__option" onClick={handleAuthentication}>
             <span className="header__optionLineOne">
-              Hello {user ? user.email : "Guest"}
+              Hello {user ? user.email.split("@")[0] : "Guest"}
             </span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
           </div>
         </Link>
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
-        </div>
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
--- src/Orders.js
+++ src/Orders.js
@@ -1,9 +1,41 @@
-import React from "react";
-import "./Orders";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
+import Order from "./Order";
+import "./Orders.css";
+import { useStateValue } from "./StateProvider";
+
 function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      onSnapshot(
+        query(
+          collection(db, "users", user?.uid, "orders"),
+          orderBy("created", "desc")
+        ),
+        (snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+      );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
   return (
     <div className="orders">
-      <h1>Orders</h1>
+      <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   );
 }
