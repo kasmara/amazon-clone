70e0665d1c0c9d5aa0d39077350604b621b69f35
Cascadio <briankasmara@yahoo.co.nz>
2021-09-25 21:52:27 +1200

    Completed amazon clone v1

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
@@ -14,7 +15,7 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// API route
+// API
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
 app.post("/payments/create", async (request, response) => {
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-14ca4/us-central1/api", // the API url (cloud function url)
+  baseURL: "https://us-central1-clone-14ca4.cloudfunctions.net/api",
+  //   "http://localhost:5001/clone-14ca4/us-central1/api"
 });
 
 export default instance;
